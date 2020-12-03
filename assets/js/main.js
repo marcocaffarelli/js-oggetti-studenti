131cb13a2dfe65254cfba476522aff0daa59435f
marcocaffarelli <caffarelli-m94@hotmail.it>
2020-12-03 13:55:38 +0100

    Creato l'oggetto studente
    Creato l'oggetto studente

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,13 @@
-// Creare un oggetto che descriva uno studente con le seguenti proprietà: nome, cognome e età. Stampare a schermo attraverso il for in tutte le proprietà.
+// Stampare a schermo attraverso il for in tutte le proprietà.
 // Creare un array di oggetti di studenti. Ciclare su tutti gli studenti e stampare per ognuno nome e cognome
 // Dare la possibilità all’utente attraverso 3 prompt di aggiungere un nuovo oggetto studente inserendo nell’ordine: nome, cognome e età.
 
 $(function (){
-    
-
-}
+    //Creare un oggetto che descriva uno studente con le seguenti proprietà: nome, cognome e età.
+    var studente = {
+        nome : "Marco",
+        cognome : "Caffarelli",
+        età : "26"
+    };
+    console.log(studente);
+});
