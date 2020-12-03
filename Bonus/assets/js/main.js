69633eff5073191919094d6ad8214a02f9ee0877
marcocaffarelli <caffarelli-m94@hotmail.it>
2020-12-03 18:28:09 +0100

    Stampato anche studente nuovo nell'html
    Stampato anche studente nuovo nell'html

--- Bonus/assets/js/main.js
+++ Bonus/assets/js/main.js
@@ -3,9 +3,9 @@ $(function (){
     var studente = {
         Nome : "Marco",
         Cognome : "Caffarelli",
-        Eta : "26"
+        Età : "26"
     };
-    console.log(studente);
+    //console.log(studente);
 
     //Stampare a schermo attraverso il for in tutte le proprietà.
     for (var key in studente) {
@@ -19,60 +19,57 @@ $(function (){
         {
             Nome : "Francesco",
             Cognome : "Totti",
-            Eta : 44
+            Età : 44
         },
         {
             Nome : "Daniele",
             Cognome : "De Rossi",
-            Eta : 37
+            Età : 37
         },
         {
             Nome : "Alessandro",
             Cognome : "Florenzi",
-            Eta : 29
+            Età : 29
         },
         {
             Nome : "Edin",
             Cognome : "Dzeko",
-            Eta : 34
+            Età : 34
         },
         {
             Nome : "Lorenzo",
             Cognome : "Pellegrini",
-            Eta : 24
+            Età : 24
         }
     ]
 
     // Ciclare su tutti gli studenti e stampare per ognuno nome e cognome.
-    for (var key in studenti) {
-        //console.log(key, studenti[key]);
-        
-    };
+    //for (var key in studenti) {
+        //console.log(key, studenti[key]); 
+    //};
+
+    // ciclo for per stamapre gli oggetti dell'array nell'html
     for (var i = 0; i < studenti.length; i++) {
         //console.log(studenti[i]); 
-        document.getElementById('lista').innerHTML += '<li>' + 'Nome: ' + studenti[i].Nome + '<br>' + 'Cognome: ' + studenti[i].Cognome + '<br>' + 'Età: ' + studenti[i].Eta + '<br>' + '</li>'; 
-    }
+        document.getElementById('lista').innerHTML += '<li>' + 'Nome: ' + studenti[i].Nome + '<br>' + 'Cognome: ' + studenti[i].Cognome + '<br>' + 'Età: ' + studenti[i].Età + '<br>' + '</li>'; 
+    };
 
 
 
     // lista dei dati che inserisce l'utente tramite prompt
-    /*var nomeUtente = prompt("Inserisci il tuo nome");
+    var nomeUtente = prompt("Inserisci il tuo nome");
     var cognomeUtente = prompt("Inserisci il tuo cognome");
     var etaUtente = Number(prompt("Inserisci la tua età"));
 
     // oggetto studente nuovo che racchiude i prompt inseriti dall'utente 
     var studenteNuovo = {
-        nome : nomeUtente,
-        cognome : cognomeUtente,
-        eta : etaUtente
+        Nome : nomeUtente,
+        Cognome : cognomeUtente,
+        Età : etaUtente
     }
     //console.log(studenteNuovo);
+    for (var key in studenteNuovo) {
+        document.getElementById('utente').innerHTML += key + ": " + studenteNuovo[key] + '<br>';
+    };
 
-    // aggiunto lo studente nuovo alla lista dei studenti
-    studenti.push(studenteNuovo);
-
-    // ciclo for in per verificare se studenteNuovo è stato aggiunto alla lista studenti
-    for (key in studenti) {
-        //console.log(key, studenti[key]);
-    };*/
 });
